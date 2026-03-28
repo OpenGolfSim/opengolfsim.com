535b4f76da12a2ae33776af1c654e751cd887409
Brian Robinson <brian@ndmweb.com>
2026-03-28 12:46:58 -0600

    fix: update plugin docs

--- apis/plugins.d.ts
+++ apis/plugins.d.ts
@@ -12,99 +12,9 @@
  *   - Standard JS: Only ECMAScript built-ins (e.g., `JSON`, `Map`, `Math`) are present.
  * 
  * 
- * ## Install Plugins
- * To install an new plugin, copy the provided plugin to your OpenGolfSim `plugins/` folder.
  * 
- * ### Plugin Folder Location
+ * Learn more in our plugin guide at: https://help.opengolfsim.com/desktop/plugins/
  * 
- * #### Windows
- * 
- * On Windows, the plugins folder should be found at:
- * 
- * ```
- * %USERPROFILE%\AppData\Roaming\opengolfsim-desktop\plugins\your-custom-plugin
- * ```
- * 
- * #### MacOS
- * 
- * On MacOS, the plugins folder should be found at:
- * 
- * ```
- * ~/Library/Application Support/opengolfsim-desktop/plugins/your-custom-plugin
- * ```
- * 
- * ## Development
- * 
- * To create a new custom plugin, create a new folder in the OpenGolfSim `plugins/` folder (see above) and create the following two files inside it:
- * 
- * 1. `package.json` - A metadata file that contains information about your plugin in JSON format
- * 
- *     Here is an example `package.json` file:
- * 
- *     ```json
- *     {
- *       "name": "Connector API",
- *       "version": "1.0.0",
- *       "description": "Creates a simple TCP server to allow connectors to send and receive shot data",
- *       "plugin": {
- *         "type": "launch"
- *       }
- *     }
- *     ```
- * 
- * 1. `index.js` - This file will contain all your javascript plugin code. When your plugin is started we'll execute the code in this file.
- * 
- * @example
- * /// <reference path="https://help.opengolfsim.com/apis/plugins/plugins.d.ts" />
- * const PORT = 3921;
- * 
- * const device = { isConnected: false, isReady: false };
- * 
- * const server = network.createServer((socket) => {
- *   // A TCP client has connected
- *   device.isConnected = true;
- *   launchMonitor.updateDeviceStatus(device);
- * 
- *   socket.on('data', (data) => {
- *     // A message has been received
- *     logging.info(`Socket data received`);
- *     try {
- *       // Parse the JSON payload
- *       const obj = JSON.parse(data);
- *       if (obj.type === 'device') {
- *         // Set the status to ready
- *         launchMonitor.setReady(obj.status === 'ready');
- *       } else if (obj.type === 'shot') {
- *         // Send the shot data in OpenGolfSim format
- *         launchMonitor.sendShot({
- *           ballSpeed: obj.shot.speed,
- *           verticalLaunchAngle: obj.shot.vla,
- *           horizontalLaunchAngle: obj.shot.hla,
- *           spinSpeed: obj.shot.totalspin,
- *           spinAxis: obj.shot.spinaxis
- *         });
- *       }
- * 
- *     } catch (error) {
- *       console.error(error);
- *     }
- *   });
- * 
- *   socket.on('end', () => {
- *     console.log(`Socket ended`);
- *     device.isConnected = false;
- *     launchMonitor.updateDeviceStatus(device);
- *   });
- * 
- *   socket.on('error', (err) => {
- *     console.error(`Socket error: ${err}`);
- *   });
- * });
- * 
- * console.log(`Starting TCP server...`);
- * server.listen(PORT, () => {
- *   console.log(`TCP server listening at 127.0.0.1:${PORT}`);
- * });
  */
 
 declare global {
@@ -114,9 +24,15 @@ declare global {
     function info(...args: any[]): void
     function error(...args: any[]): void
   }
+  
+  /** @namespace */
+  namespace system {
+    /** Called before the plugin exits to allow for graceful shutdown */
+    function on(eventName: 'exit', listener: () => Promise<void>): void
+  }
 
   /**
-   * Send and re
+   * Send and receive shot data from OpenGolfSim
    * @namespace
    *
    */
